c958bb088e1a95febff8fd766a9af09bb547e418
Matt Robison <mrobison@iprosystems.com>
2019-10-10 12:41:26 -0500

    Added routes to app

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def getTeam(id=None):
     data = models.Teams.query.get(id)
     return jsonify(utils.row2dict(data))
 
-@app.route('/getPlayers/', methods=['GET'])
+@app.route('/getPlayers', methods=['GET'])
 @app.route('/getPlayers/<team_id>', methods=['GET'])
 def getPlayers(team_id=None):
     if not team_id:
@@ -37,31 +37,39 @@ def getPlayers(team_id=None):
 
     return jsonify(utils.row2dict(data))
 
-@app.route('/addPlayer/', methods=['POST'])
+@app.route('/addPlayer', methods=['POST'])
 def addPlayers():
     results = request.get_json()
+    user = request.args.get('player')
     app.logger.info(results)
 
 
-@app.route('/updatePlayer/', methods=['PUT'])
+@app.route('/updatePlayer', methods=['PUT'])
 def updatePlayers():
     results = request.get_json()
     app.logger.info(results)
 
-@app.route('/addGame/', methods=['POST'])
+
+@app.route('/addCoach', methods=['POST'])
+def addPlayers():
+    results = request.get_json()
+    user = request.args.get('coach')
+    app.logger.info(results)
+
+@app.route('/addGame', methods=['POST'])
 def addGame():
     results = request.get_json()
     app.logger.info(results)
 
 @app.route('/getStandings', methods=['GET'])
-@app.route('/getStandings/<division>')
-def getStandings(division=None):
+@app.route('/getStandings/<year>/<level>')
+def getStandings(year=None,level=None):
     if division:
         pass
 
 # @app.route('/getScheduleBy')
-@app.route('/getScheduleBy/<type>/<id>')
-def getSchedule(type=None, id=None):
+@app.route('/getScheduleBy/<year>/<level>/<id>')
+def getSchedule(year=None, level=None):
     if lower(type) == 'year':
         # Query the schedule table with year as the filter
         pass
