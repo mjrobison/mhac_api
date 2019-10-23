5299217b3edd475bc3d7a2211e49e8778d1a441f
Matt Robison <baseballrocks0412@gmail.com>
2019-10-23 14:28:55 -0500

    schedule

--- app.py
+++ app.py
@@ -162,7 +162,7 @@ def getStandings(season_id=None):
     if not season_id:
         return
 
-    schedule = models.Schedule.query.filter(models.Schedule.season_id == season_id)
+    standings = models.Standings.query.filter(models.Schedule.season_id == season_id)
 
 
 @app.route('/addGame', methods=['POST'])
@@ -216,47 +216,33 @@ def addGame():
 
     return "Game added to the schedule", 200
 
+
 @app.route('/getSchedule/<season_id>/', methods=['GET'])
-def getSchedule(season_id=None, team_id):
+def getSchedule(season_id=None, team_id=None):
     home_team = aliased(models.Teams, name='home_team')
     away_team = aliased(models.Teams, name='away_team')
-    #schedule = models.Schedule.query.filter(models.Schedule.season_id == season_id)
-    # results = db.session.query(mdels.Schedule, models.Games, models.Teams).filter(models.Schedule.season_id == season_id).filter(mode)
-    results = db.session.query(models.Schedule).join(models.Games).join(home_team, models.Games.home_team_id == home_team.id).join(away_team, models.Games.away_team_id == away_team.id)
+    query = db.session.query(models.Schedule, models.Games, home_team, away_team)
+    if season_id:
+        query = db.session.query(models.Schedule, models.Games, home_team, away_team).filter(models.Schedule.season_id == season_id)
+    results = query.all()
     data_all = []
 
-    data = {
-        "away_score": "None",
-        "away_team_id": {
-          "id": "8b31d1b6-e233-11e9-a4c2-b827ebcfd443",
-          "name": "Hendersonville Royals"
-        },
-        "game_date": "2019-11-19",
-        "game_time": "7:00pm",
-        "home_score": "None",
-        "home_team_id": {
-          "id": "8b31d3fa-e233-11e9-a4c2-b827ebcfd443",
-          "name": "Tennessee Heat"
-        },
-        "id": "d9f5564c-ee31-11e9-88d3-b827ebcfd443",
-        "neutral_site": "None",
-        "season_id": {
-          "id": "0182b606-ee31-11e9-b8a6-b827ebcfd443",
-          "name": "Boys 18u Basketball"
-        }
-    data_all.append(data)
+    for r in results:
+        data = {}
+        data['id'] = r.Schedule.id
+        data['game_date'] = r.Schedule.game_date
+        data['game_time'] = r.Schedule.game_time
+        home_team = {}
+        home_team['id'] = r.home_team.id
+        home_team['name'] = r.home_team.team_name
+        data['home_team'] = home_team
+        away_team = {}
+        away_team['id'] = r.away_team.id
+        away_team['name'] = r.away_team.team_name
+        data['away_team'] = away_team
+        data_all.append(data)
 
-    # for r in results:
-    #     data = {}
-    #     data['id'] = r.Schedule.id
-    #     data['game_date'] = r.Schedule.game_date
-    #     data['game_time'] = r.Schedule.game_time
-    #     home_team = {}
-    #     home_team['id'] = r.Teams.
-    #     data[]
-    #     data_all.append(data)
-    
-    return jsonify(results), 200
+    return jsonify(data_all), 200
 
 @app.route('/getSeasons', methods=['GET'])
 def getSeason():
