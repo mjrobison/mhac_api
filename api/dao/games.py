f623d8752a35e06301aaae9ebc5838642cd12c70
Matt Robison <baseballrocks0412@gmail.com>
2020-11-21 23:29:21 -0600

    Updating the stats call

--- api/dao/games.py
+++ api/dao/games.py
@@ -694,3 +694,104 @@ def add_games_and_stats(game: GameStats):
         print(str(exc))
     finally:
         DB.close()
+
+
+def stats_by_season_and_team(season_id, team_id):
+    DB = db()
+    base_query = text("""SELECT 
+    st.season_id, player_id, number AS player_number, bs.team_id, p.first_name, p.last_name, t.team_name
+                , SUM(field_goals_attempted) AS field_goals_attempted
+                , SUM(field_goals_made) AS field_goals_made
+                , SUM(three_pointers_attempted) AS three_pointers_attempted
+                , SUM(three_pointers_made) AS three_pointers_made
+                , SUM(free_throws_attempted) AS free_throws_attempted
+                , SUM(free_throws_made) AS free_throws_made
+                , SUM(total_points) AS total_points
+                , SUM(assists) AS assists
+                , SUM(offensive_rebounds) AS offensive_rebounds
+                , SUM(defensive_rebounds) AS defensive_rebounds
+                , SUM(total_rebounds) AS total_rebounds
+                , SUM(steals) AS steals
+                , SUM(blocks) AS blocks
+                , SUM(turnovers) AS turnovers
+                , COUNT(game_id) AS games_played
+            FROM mhac.basketball_stats aS bs
+            INNER JOIN mhac.season_teams AS st
+                ON bs.team_id = st.id
+            INNER JOIN mhac.teams AS t
+                ON st.team_id = t.id
+            INNER JOIN mhac.person AS p
+                ON bs.player_id = p.id
+            
+    """)
+    group_by = """GROUP BY st.season_id, player_id, bs.team_id, p.first_name, p.last_name, t.team_name, number"""
+     
+    if season_id and team_id:
+        where = '''WHERE st.season_id = :season_id and st.id = :team_id '''
+        stmt = text(f'''{base_query} 
+                       {where} 
+                       {group_by} ''')
+        stmt = stmt.bindparams(season_id=season_id, team_id = team_id)               
+        
+    elif season_id:
+        where = '''WHERE st.season_id = :season_id '''
+        stmt = text(f'''{base_query} 
+                       {where} 
+                       {group_by} ''')
+        stmt = stmt.bindparams(team_id=team_id)               
+
+    elif team_id:
+        where = ''' WHERE st.id = :team_id'''
+        stmt = text(f'''{base_query} 
+                       {where} 
+                       {group_by} ''')
+        stmt = stmt.bindparams(team_id = team_id)
+
+    results = DB.execute(stmt)
+
+    stats = {}
+    stats['season_id'] = season_id
+    stats['team_id'] = team_id
+
+    for r in results:
+        field_goal_percentage = 0.0
+        if r.field_goals_attempted != 0:
+            field_goal_percentage = float(r.field_goals_made)/float(r.field_goals_attempted)
+
+        three_point_percentage = 0.0
+        if r.three_pointers_attempted != 0:
+            three_point_percentage = float(r.three_pointers_made)/float(r.three_pointers_attempted)
+
+        free_throw_percentage = 0.0
+        if r.free_throws_attempted != 0:
+            free_throw_percentage = float(r.free_throws_made)/float(r.free_throws_attempted)
+        data = {
+            "team_id": r.team_id,
+            "team_name": r.team_name,
+            "player_first_name": r.first_name,
+            "player_last_name": r.last_name,
+            "player_number": r.player_number,
+            "player_id": r.player_id,
+            "player_stats": {
+                "2PA": r.field_goals_attempted,
+                "2PM": r.field_goals_made,
+                '2P%': field_goal_percentage,
+                "3PA": r.three_pointers_attempted,
+                "3PM": r.three_pointers_made,
+                "3P%": three_point_percentage, 
+                "FTA": r.free_throws_attempted,
+                "FTM": r.free_throws_made,
+                "FT%": free_throw_percentage, 
+                "total_points": r.total_points,
+                "assists": r.assists,
+                "offensive_rebounds": r.offensive_rebounds,
+                "defensive_rebounds": r.defensive_rebounds,
+                "total_rebounds": r.total_rebounds,
+                "steals": r.steals,
+                "blocks": r.blocks,
+                "turnovers": r.turnovers,
+                "games_played": r.games_played,
+                "points_per_game": float(r.total_points)/float(r.games_played)
+            }
+        }
+        data_all.append(data)
--- api/dao/stats.py
+++ api/dao/stats.py
@@ -69,8 +69,10 @@ def stats_by_season_and_team(season_id, team_id):
             
     """)
     group_by = """GROUP BY st.season_id, player_id, bs.team_id, p.first_name, p.last_name, t.team_name, number"""
-     
+    print(season_id)
+    print(team_id)
     if season_id and team_id:
+        print("Here")
         where = ''' AND st.season_id = :season_id AND st.id = :team_id '''
         stmt = text(f'''{base_query} 
                        {where} 
@@ -78,13 +80,15 @@ def stats_by_season_and_team(season_id, team_id):
         stmt = stmt.bindparams(season_id=season_id, team_id = team_id)               
         
     elif season_id:
+        print("HEre")
         where = '''AND st.season_id = :season_id '''
         stmt = text(f'''{base_query} 
                        {where} 
                        {group_by} ''')
-        stmt = stmt.bindparams(team_id=team_id)               
+        stmt = stmt.bindparams(season_id=season_id)               
 
     elif team_id:
+        print("Last Here")
         where = ''' AND st.id = :team_id'''
         stmt = text(f'''{base_query} 
                        {where} 
