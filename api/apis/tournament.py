e2c16b9fa1781d380fa0ccc8c2ff4d287e11f5b5
Matt Robison <mrobison@flogistix.com>
2021-01-13 08:55:54 -0600

    Updates for tournament

--- api/apis/tournament.py
+++ api/apis/tournament.py
@@ -4,12 +4,26 @@ from pydantic import BaseModel, ValidationError, validator
 from uuid import UUID
 from datetime import datetime, date, time
 
+from dao import tournament
+
 router = APIRouter()
+
 
 @router.get('/getTournamentInformation', tags=['tournament'])
 def get_tournament_games():
-    return {'games': []}
+    # print(tournament.get_tournament_games())
+    return {'games': tournament.get_tournament_games()}
+    # return games.get_tournament_games()
 
 @router.post('/addTournamentGame', tags=['games'])
 def add_tournament_game():
     pass
+
+@router.get('/getActiveTournaments', tags=['tournament'])
+def get_active_tournament():
+    print(tournament.get_tournament())
+    return tournament.get_tournament()
+
+@router.get('/getTournaments', tags=['tournament'])
+def get_tournaments(year):
+    return tournament.get_tournament(year=year)
--- api/dao/tournament.py
+++ api/dao/tournament.py
@@ -9,5 +9,84 @@ from datetime import date, timedelta, datetime, time
 from database import db
 import csv
 
+from .seasons import get_by_id
 
 import json
+
+class MatchUp(TypedDict):
+    team1 = str
+    scoreTeam1 = int
+    team2 = str
+    scoreTeam2 = int
+
+class Location(TypedDict):
+    address = str
+    name = str
+
+class TournamentGame(TypedDict):
+    game = int
+    date = date
+    time = time
+    matchup = MatchUp
+    location = Location
+    level = str
+
+def tournamentGameRowMapper(row) -> TournamentGame:
+    TournamentGame = {
+        'game': row['game_number'],
+        'date': row['game_date'],
+        'time': row['game_time'],
+        'matchup': {
+            'team1': row['home_team'],
+            'scoreTeam1': row['home_team_score'],
+            'team2': row['away_team'],
+            'scoreTeam2': row['away_team_score']
+        },
+        'location': {
+            'address': '',
+            'name': ''
+        },
+        'level': row['level_name']
+    }
+    return TournamentGame
+
+def tournamentRowMapper(row):
+    return {
+        'tournament_id': row['tournament_id'],
+        'season': get_by_id(row['tournament_season']),
+        'year': row['tournament_year']
+    }
+
+
+def get_tournament_games() -> TournamentGame:
+    DB = db()
+    query = '''
+    SELECT game_number, game_date, game_time, home_team.team_name as home_team, away_team.team_name as away_team, home_team_score, away_team_score, '' as game_location, levels.level_name
+    FROM mhac.tournamentgames
+    INNER JOIN mhac.seasons
+        ON tournamentgames.season_id = seasons.id
+    INNER JOIN mhac.levels 
+        ON levels.id = seasons.level_id
+    INNER JOIN mhac.season_teams_with_names as home_team
+        ON tournamentgames.home_team = home_team.id
+    INNER JOIN mhac.season_teams_with_names as away_team
+        ON tournamentgames.away_team = away_team.id
+    WHERE year = '2020'
+    '''
+    
+    data_all=[]
+    results = DB.execute(query)
+    for r in results:
+        data_all.append(tournamentGameRowMapper(r))
+        
+    return data_all
+
+
+def get_tournament(DB=db(), year=None):
+    query = """SELECT * FROM mhac.tournaments"""
+    data_all = []
+    results = DB.execute(query)
+    for r in results:
+        data_all.append(tournamentRowMapper(r))
+    
+    return data_all
