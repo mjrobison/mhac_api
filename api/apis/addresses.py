e1c5d396b2f8cc511330a45c5ee34a1dba710190
Matt Robison <mrobison@flogistix.com>
2021-02-05 10:49:06 -0600

    Adding addresses

---
+++ api/apis/addresses.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter, HTTPException
+from typing import Optional, List, Dict
+from pydantic import BaseModel
+from uuid import UUID
+from datetime import datetime, date
+
+
+
+class Address(BaseModel):
+    address_id: UUID
+    location_name: Optional[str]
+    address_line_1: str
+    address_line_2: Optional[str]
+    city: str
+    state: str
+    postal_code: str
