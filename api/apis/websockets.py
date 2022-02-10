ed58d6acadeeba147a84731f541a851fde3c39c9
Matt Robison <mrobison@flogistix.com>
2022-02-10 11:46:24 -0600

    Changing port scheme

--- api/apis/websockets.py
+++ api/apis/websockets.py
@@ -21,8 +21,12 @@ def get_websocket_url() -> WebSocketLocation:
     results = websockets.get_websocket_url()
     if not results:
         results = {'websocket_url': f"wss://localhost", "websocket_port": 4444}
-
-    return {'webSocketUrl': f"{results['websocket_url']}:{results['websocket_port']}"}
+    
+    returnobj = {'webSocketUrl': f"{results['websocket_url']}:{ results['websocket_port']}"}
+    if results['websocket_port'] == 80:
+        returnobj = {'webSocketUrl': f"{results['websocket_url']}"}
+    
+    return returnobj
 
 @router.post('/websocketUrl',  status_code=201, tags=['obs', 'livestream'])
 def post_websocket_url(websocket_url: WebSocketModel):
